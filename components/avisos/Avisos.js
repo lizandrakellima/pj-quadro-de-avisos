f15dbfc263750a5693483a210f13081fb92a64a5
Lizandra Kelly de Lima <lizandrakdelima@gmail.com>
2020-12-16 03:25:25 -0300

    Selecionar e alterar um aviso

--- components/avisos/Avisos.js
+++ components/avisos/Avisos.js
@@ -20,6 +20,23 @@ function salvar(aviso){
 }//fim do salvar
 
 /**
+ * Alterar um aviso no banco de dados
+ * @param {object} aviso  O aviso deve estar no seguinte formato:
+ * {titulo: <string>, data <date>, mensagem <string>}
+ * @param {int} id ID do aviso
+ * @returns {object} Mensagem de sucesso ou de erro
+ */
+function editar(aviso, id){
+  return db('avisos').where('ID_avisos', id).update(aviso)
+  .then( _ =>{
+    return {tipo: "Sucesso ", corpo: "Aviso alterado com sucesso!"}
+  })
+  .catch(erro =>{
+    return {tipo: "Erro ", corpo: "Erro: "+ erro}
+  })
+} // fim do editar
+
+/**
  * Seleciona todos os avisos cadastrados
  * @returns {object} Objeto com todos os avisos cadastrados ou
  * uma mensagem de erro 
@@ -33,14 +50,28 @@ function selecionarTodos(){
     })
 }// fim do selecionarTodos
 
+
+/**
+ * Seleciona um aviso
+ * @param {*} id ID do aviso selecionada
+ * @returns {object} Objeto com o aviso selecionado
+ */
+function selecionarAviso(id){
+  return db.select().from('avisos').where('ID_avisos', id).first()
+  
+  .then(aviso => {return aviso})  
+    .catch(erro =>{
+      return { tipo: "erro", corpo: "Erro: " + erro }
+    })
+}// fim do selecionarAviso
+
 /**
  * Função que exclui um avuiso do banco de dados
  * @param {*} id Id do aviso
  */
 
 function excluir(id){
-return db.del().from('avisos').where('ID_avisos',id)
-
+return db.del().from('avisos').where('ID_avisos', id)
 }
 
-module.exports = {salvar, selecionarTodos, excluir}
+module.exports = {salvar, selecionarTodos, selecionarAviso, editar, excluir}
--- components/avisos/AvisosController.js
+++ components/avisos/AvisosController.js
@@ -5,22 +5,27 @@ const router = require('express').Router()
 
 const Avisos = require('./Avisos')
 
-router.get('/',(req, res)=> { 
+router.get("/",(req, res)=> { 
   res.send("Pag Principal")
 })
 
-router.get('/avisos', async (req, res)=> { 
-  //res.send("Avisos Cadastrados")
-
+router.get("/avisos", async (req, res)=> { 
   const avisos = await Avisos.selecionarTodos()
   res.render('avisos',{avisos})
 })
 
-router.get('/avisos/novo',(req, res)=> { 
+router.get("/avisos/novo",(req, res)=> { 
   res.render('form_avisos')
 })
 
-router.post('/avisos/novo', async (req, res)=> { 
+router.get("/avisos/editar/:id", async (req, res) =>{
+  const id = req.params.id
+  const aviso = await Avisos.selecionarAviso(id)
+  res.render('form_avisos', {aviso})
+
+})
+
+router.post("/avisos/novo", async (req, res)=> { 
   const titulo = req.body.titulo
   const data = req.body.data
   const mensagem = req.body.mensagem
@@ -30,7 +35,24 @@ router.post('/avisos/novo', async (req, res)=> {
 
 })
 
-router.get('/avisos/excluir/id', async (req, res)=> {
+router.post("/avisos/editar/:id", async (req, res)=> {
+  const id = req.body.id
+  const titulo = req.body.titulo
+  const data = req.body.data
+  const mensagem = req.body.mensagem
+
+  const msg = await Avisos.editar({titulo, data, mensagem}, id)
+
+  if (msg.tipo === "sucesso"){
+    res.redirect('/avisos')
+  }
+  else {
+    res.render('form_avisos', {msg})
+  }
+  
+})
+
+router.get("/avisos/excluir/:id", async (req, res)=> {
   const id = Number(req.params.id)
   await Avisos.excluir(id)
   res.redirect('/avisos')
